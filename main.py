ea8cdbe614d83abdbed16a44421e20e2f76f1b8a
Pablo Sagarna <pablosa@wso2.com>
2026-08-03 15:03:12 +0200

    .

--- main.py
+++ main.py
@@ -228,4 +228,4 @@ if __name__ == "__main__":
     host = os.getenv("API_HOST", "127.0.0.1")
     port = int(os.getenv("API_PORT", 8000))
     
-    uvicorn.run("main:app", host=host, port=port, reload=True)
+    uvicorn.run("main:app", host=host, port=port)
